89e42c857cf820329a2888400aaa8b2fcf9325e4
renprimucci <140030571+renprimucci@users.noreply.github.com>
2023-09-03 20:39:20 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+let nombre=[]
+let bandera = true
+
+function saludo () {
+while(bandera) {
+let nombres =prompt ("ingrese su nombre")
+if (nombres != null){
+    alert("bienvenido a nuestra pagina")
+bandera = false
+} else {
+    nombre.push()
+}
+}
+}
+saludo ()
